8ffba94d32fb0f445ae80e8b4d3911d97e9f5ceb
Luzivam34 <luzivam.santos28@outlook.com>
2025-07-21 11:16:23 -0300

    criado e implamentado o arquivo config.py com a class Config

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'default_secret_key'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///tmp/app.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # Disable track modifications to save resources
